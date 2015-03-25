c2abb5079c2fb9c54ffbbce4af7a99d549632159
Callan McInally <callan@shrewburyrobotics.org>
2015-03-24 21:58:28 -0400

    Clean up refactor of the original code - intended for use as a reference to show others a different (hopefully more readable/maintainable) version of the original code.
    Note - I don't have the arduino development system installed on my laptop so I can't test that this code will actually compile. It may have syntax errors - or use language features not available on the arduino platform.

--- robotLight/robotLight.ino
+++ robotLight/robotLight.ino
@@ -1,154 +1,174 @@
-  int Gi = 10;
-  int Ri = 11;
-  int Bi = 9;
-  
-  int bit0 = 0;
-  int bit1 = 1;
-  int bit2 = 2;
-  
-void setup() {
+#define GREEN_CHANNEL = 10;
+#define RED_CHANNEL = 11;
+#define BLUE_CHANNEL = 9;
+
+void setup() 
+{
   
 }
 
-void loop_debug() {
-  //debug mode that links each digital input with a color channel if things are
-  //not working as they should
+void loop_debug() 
+{
+    //debug mode that links each digital input with a color channel if things are
+    //not working as they should
 
-  if (digitalRead(bit0) == HIGH)
-  {
-     analogWrite(Ri,255); 
-  }
-  else
-  {
-    analogWrite(Ri,0);
-  }   
-  if (digitalRead(bit1) == HIGH)
-  {
-     analogWrite(Gi,255); 
-  }
-  else
-  {
-    analogWrite(Gi,0);
-  }   
-  if (digitalRead(bit2) == HIGH)
-  {
-     analogWrite(Bi,255); 
-  }
-  else
-  {
-    analogWrite(Bi,0);
-  }
+    if (digitalRead(autoOut) == HIGH)
+    {
+        analogWrite(Ri,255); 
+    }
+    else
+    {
+        analogWrite(Ri,0);
+    }   
+    if (digitalRead(teleopOut) == HIGH)
+    {
+        analogWrite(Gi,255); 
+    }
+    else
+    {
+        analogWrite(Gi,0);
+    }   
+    if (digitalRead(warningOut) == HIGH)
+    {
+        analogWrite(Bi,255); 
+    }
+    else
+    {
+        analogWrite(Bi,0);
+    }
+}
+
+/* 
+ * Set the LED strip to the specified RGB values
+ * 
+ */
+void RGBWrite(int red, int green, int blue)
+{
+    analogWrite(RED_CHANNEL, red);
+    analogWrite(GREEN_CHANNEL, green);
+    analogWrite(BLUE_CHANNEL, blue);
+}
+
+/*
+ * set a given RGB color then wait for the specified delay
+ * 
+ * red, green, blue - bytes in range 0 to 255
+ * delay - delay in milliseconds to wait
+ *
+ */
+void RGBWriteDelay(int red, int green, int blue, int delay)
+{
+    RGBWrite(red, green, blue);
+    delay(delay);
+}
+
+/*
+ * pulse the given color
+ *
+ * The input red, green and blue values should be 0 or 1
+ *
+ */
+void RGBPulse(int red, int green, int blue)
+{
+    for (int i = 1; i < 255; i++) 
+    {
+        // Ramp up brightness
+        RGBWrite(red*i, green*i, blue*i); 
+        delay(1);
+    }
+    for (int i = 255; i > 1; i--) 
+    {
+        // Ramp down brightness
+        RGBWrite(red*i, green*i, blue*i);
+        delay(3);
+    }
+}
+
+/*
+ * Fast pulse a given color
+ *
+ * The input red, green and blue values should be 0 or 1
+ *
+ */
+void RGBFastPulse(int red, int green, int blue)
+{
+    for (int i = 1; i < 255; i+=2) 
+    {
+        // Ramp up brightness
+        RGBWrite(red*i, green*i, blue*i); 
+        delay(1);
+    }
+    for (int i = 255; i > 1; i--) 
+    {
+        // Ramp down brightness
+        RGBWrite(red*i, green*i, blue*i);
+        delay(3);
+    }
+}
+
+/*
+ * These enums should be the same as used on the Java side for the 
+ * robot code
+ *
+ */
+typedef enum 
+{
+    OFF           = 0,
+    RAINBOW       = 1,
+    PULSE_RED     = 2,
+    PULSE_BLUE    = 3,
+    BLUE_AND_GOLD = 4,
+    YELLOW        = 6,
+} LEDMode;
+
+/*
+ * Different modes are encoded into 3 digital output channels from the RoboRio
+ * map these to the enum
+ *
+ */
+LEDMode readInput()
+{
+    int b0 = (digitalRead(0) == HIGH) ? 0x01 | 0;
+    int b1 = (digitalRead(1) == HIGH) ? 0x02 | 0;
+    int b2 = (digitalRead(2) == HIGH) ? 0x04 | 0;
+
+    return ( b0 | b1 | b2);
 }
 
 void loop()
 {
-  
-
-
-  if (digitalRead(bit0) == HIGH && digitalRead(bit1) == LOW && digitalRead(bit2) == LOW) {
-    //rainbow mode that switches colors every 100 milleseconds, occurs during autonomous mode
+    switch (readInput())
+    {
+        case RAINBOW:
+            //rainbow mode that switches colors every 100 milleseconds
     
-      analogWrite(Ri, 255);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, 0);
-      delay(100);
-      analogWrite(Ri, 255);
-      analogWrite(Gi, 255);
-      analogWrite(Bi, 0);
-      delay(100);
-      analogWrite(Ri, 0);
-      analogWrite(Gi, 255);
-      analogWrite(Bi, 0);
-      delay(100);
-      analogWrite(Ri, 0);
-      analogWrite(Gi, 255);
-      analogWrite(Bi, 255);
-      delay(100);
-      analogWrite(Ri, 0);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, 255);
-      delay(100);
-      analogWrite(Ri, 255);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, 255);
-      delay(100);
-  }
+            RGBWriteDelay(255, 0, 0, 100);    // Red for 100 ms
+            RGBWriteDelay(255, 255, 0, 100);  // Yellow for 100ms
+            RGBWriteDelay(0, 255, 0, 100);    // Green for 100ms
+            RGBWriteDelay(0, 255, 255, 100);  // Cyan for 100ms     
+            RGBWriteDelay(0, 0, 255, 100);    // Blue for 100ms
+            RGBWriteDelay(255, 0, 255, 100);  // Magenta for 100ms
+            break;
 
- else if (digitalRead(bit0) == LOW && digitalRead(bit1) == HIGH && digitalRead(bit2) == LOW) {
-   //pulse of red that occurs during teleop while we are on the red alliance
-      for (int i = 1; i <255; i++) {
-      analogWrite(Ri, i);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, 0);
-      delay(1);
-      }
-      for (int i = 255; i > 1; i--) {
-      analogWrite(Ri, i);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, 0);
-      delay(3);
-      }  
-  }
- else if (digitalRead(bit0) == HIGH && digitalRead(bit1) == HIGH && digitalRead(bit2) == LOW) {
-   //pulse of blue that occurs during teleop while we are on the blue alliance
-      for (int i = 1; i <255; i++) {
-      analogWrite(Ri, 0);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, i);
-      delay(1);
-      }
-      for (int i = 255; i > 1; i--) {
-      analogWrite(Ri, 0);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, i);
-      delay(3);
-      } 
-  }
-  else if (digitalRead(bit0) == LOW && digitalRead(bit1) == LOW && digitalRead(bit2) == HIGH) {
-    //pulse of yellow then blue that occurs while disabled
-      for (int i = 1; i <255; i++) {
-      analogWrite(Ri, i);
-      analogWrite(Gi, i);
-      analogWrite(Bi, 0);
-      delay(1);
-      }
-      for (int i = 255; i > 1; i--) {
-      analogWrite(Ri, i);
-      analogWrite(Gi, i);
-      analogWrite(Bi, 0);
-      delay(3);
-      }
-     for (int i = 1; i <255; i++) {
-      analogWrite(Ri, 0);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, i);
-      delay(1);
-      }
-      for (int i = 255; i > 1; i--) {
-      analogWrite(Ri, 0);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, i);
-      delay(3);
-      } 
-  }
-  else if (digitalRead(bit0) == LOW && digitalRead(bit1) == HIGH && digitalRead(bit2) == HIGH) {
-    //pulse of yellow that occurs in the last 20 sec. of teleop
-      for (int i = 1; i <255; i+=2) {
-      analogWrite(Ri, i);
-      analogWrite(Gi, i);
-      analogWrite(Bi, 0);
-      delay(1);
-      }
-      for (int i = 255; i > 1; i--) {
-      analogWrite(Ri, i);
-      analogWrite(Gi, i);
-      analogWrite(Bi, 0);
-      delay(1);
-      }
-  } 
-  else if (digitalRead(bit0) == LOW && digitalRead(bit1) == LOW && digitalRead(bit2) == LOW) {
-      analogWrite(Ri, 0);
-      analogWrite(Gi, 0);
-      analogWrite(Bi, 0);
-  }
+        case PULSE_RED:
+            RGBPulse(1, 0, 0);  // Pulse Red
+            break;
+
+        case PULSE_BLUE:
+            RGBPulse(0, 0, 1);  // Pulse Blue
+            break;
+
+        case BLUE_AND_GOLD:
+            RGBPulse(1, 1, 0);  // Pulse Yellow
+            RGBPulse(0, 0, 1);  // Pulse Blue
+            break;
+
+        case YELLOW:
+            RGBFastPulse(1, 1, 0);  // Pulse Yellow
+            break;
+
+        default:
+            RGBWrite(0, 0, 0);
+            break;
+    }
 }
